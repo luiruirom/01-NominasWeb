423f0610addce90f1508fe73c221edc2feb88b75
luiruirom <luis.ignacio.rr.1999@gmail.com>
2021-10-16 19:09:42 +0200

    Proyecto creado y configurado para poder empezar a trabajar

---
+++ src/main/java/java/es/iessoterohernandez/daw/dwes/nominas/laboral/DatosNoCorrectosException.java
@@ -0,0 +1,21 @@
+package java.es.iessoterohernandez.daw.dwes.nominas.laboral;
+
+/**
+ * @author fjdl
+ *
+ */
+public class DatosNoCorrectosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * DatosNoCorrectosExcepcion
+	 */
+	public DatosNoCorrectosException() {
+		super("Datos no correctos");
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ src/main/java/java/es/iessoterohernandez/daw/dwes/nominas/laboral/Empleado.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package java.es.iessoterohernandez.daw.dwes.nominas.laboral;
+
+/**
+ * @author fjdl
+ *
+ */
+public class Empleado extends Persona {
+
+	private int categoria=1;
+	public int anyos=0;
+	
+	/**
+	 * @param nombre
+	 * @param dni
+	 * @param sexo
+	 * @param categoria
+	 * @param anyos
+	 */
+	public Empleado(String nombre, String dni, char sexo, int categoria, int anyos) throws DatosNoCorrectosException{
+		super(nombre, dni, sexo);
+		this.setCategoria(categoria);
+		if (anyos>=0)
+			this.anyos = anyos;
+		else throw new DatosNoCorrectosException();
+	}
+
+	/**
+	 * @param nombre
+	 * @param dni
+	 * @param sexo
+	 * @throws DatosNoCorrectosException 
+	 */
+	public Empleado(String nombre, String dni, char sexo) throws DatosNoCorrectosException {
+		super(nombre, dni, sexo);
+//		this.categoria=1;
+//		this.anyos=0;
+	}
+
+	/**
+	 * @return the categoria
+	 */
+	public int getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * @param categoria the categoria to set
+	 * @throws DatosNoCorrectosException 
+	 */
+	public void setCategoria(int categoria) throws DatosNoCorrectosException {
+		if (categoria<1 || categoria>10) {
+			throw new DatosNoCorrectosException();
+		}else {
+			this.categoria = categoria;
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public void incrAnyo() {
+		this.anyos++;
+	}
+
+
+	/**
+	 *
+	 */
+	public String imprime() {
+		return "Empleado ["+ super.imprime() +", categoria=" + categoria + ", anyos=" + anyos + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
